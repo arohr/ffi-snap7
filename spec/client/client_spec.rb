b194206838fd629d0ff9ac6b599115d53a6ae12f
Patrik Wenger <patrik.wenger@mindclue.ch>
2018-02-02 13:42:02 +0100

    add basic specs

--- spec/client/client_spec.rb
+++ spec/client/client_spec.rb
@@ -3,4 +3,64 @@ RSpec.describe Snap7::Client do
   it 'initializes' do
     subject
   end
+
+  it 'creates native object' do
+    expect(Snap7).to receive(:cli_create) { FFI::MemoryPointer.new :pointer }
+    subject
+  end
+
+  describe '#connected?' do
+    it 'initializes disconnected' do
+      refute subject.connected?
+    end
+  end
+
+
+  describe '#connect' do
+    let(:address) { double }
+    let(:rack) { double }
+    let(:slot) { double }
+
+    it 'connects to default port' do
+      expect(Snap7).to receive(:cli_connect_to).with(FFI::Pointer, address, rack, slot) { 0 }
+      subject.connect address, rack, slot
+    end
+
+    it 'marks as connected' do
+      allow(Snap7).to receive(:cli_connect_to){ 0 }
+      subject.connect address, rack, slot
+
+      assert subject.connected?
+    end
+
+    it 'checks for error' do
+      expect(Snap7).to receive(:cli_connect_to) { 1 }
+      expect(Snap7).to receive(:cli_error_text).with(1, FFI::MemoryPointer, Integer) do |_, ptr, len|
+        assert_equal len, ptr.size
+        ptr.write_string 'error: foo'
+        0
+      end
+
+      assert_raises do
+        subject.connect address, rack, slot
+      end
+    end
+  end
+
+  describe '#disconnect' do
+    it 'disconnects' do
+      expect(Snap7).to receive(:cli_disconnect)
+      subject.disconnect
+    end
+
+    it 'marks as disconnected' do
+      allow(Snap7).to receive(:cli_connect_to){ 0 }
+      subject.connect double, double, double
+      assert subject.connected?
+
+      allow(Snap7).to receive(:cli_disconnect)
+      subject.disconnect
+      refute subject.connected?
+    end
+  end
 end
--- spec/server/server_spec.rb
+++ spec/server/server_spec.rb
@@ -3,5 +3,37 @@ RSpec.describe Snap7::Server do
   it 'initializes' do
     subject
   end
+
+  it 'creates native object' do
+    expect(Snap7).to receive(:srv_create) { FFI::MemoryPointer.new :pointer }
+    subject
+  end
+
+  describe '#start' do
+    it 'binds to default port' do
+      expect(Snap7).to receive(:srv_start).with(FFI::Pointer) { 0 }
+      subject.start
+    end
+
+    it 'checks for error' do
+      expect(Snap7).to receive(:srv_start) { 1 }
+      expect(Snap7).to receive(:srv_error_text).with(1, FFI::MemoryPointer, Integer) do |_, ptr, len|
+        assert_equal len, ptr.size
+        ptr.write_string 'error: foo'
+        0
+      end
+
+      assert_raises do
+        subject.start
+      end
+    end
+  end
+
+  describe '#stop' do
+    it 'stops' do
+      expect(Snap7).to receive(:srv_stop) { 0 }
+      subject.stop
+    end
+  end
 end
 
